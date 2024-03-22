2cc02d1ac740a5006a0e10ef6d37a0a725c249a5
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-03-22 17:20:19 -0500

    e

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -1,6 +1,6 @@
 // This script should NEVER include any utils related to the game itself.
 // Instead this should be used for general utils like the one below.
-function colorLog(text,color="lime",fontSize="10px"){
+function colorLog(text,color="lime",fontSize="15px"){
     console.log("%c "+text, `
         color:${color};
         font-size:${fontSize};
