a71b85a3a329f77ee19508f0c3d75264dbd183ae
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-03-25 09:54:50 -0600

    https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.youtube.com/watch%3Fv%3DdQw4w9WgXcQ&ved=2ahUKEwjOxPWB44-FAxUHDDQIHXOkC0sQtwJ6BAgjEAI&usg=AOvVaw0aHtehaphMhOCAkCydRLZU

--- src/scripts/resources.js
+++ src/scripts/resources.js
@@ -1,20 +1,29 @@
-addProperty(game, "resources", 0)
 
-var resources = {}
 
-resources.updateResourceCount = () => {
-  document.getElementById("resourceCount").textContent = "Sugar Ready For Collecting: " + FormatNumbers(game.resources);
+addProperty(game, "sugar", 0); // Add sugar property to the game object
+
+var sugar = {};
+
+sugar.updateSugarCount = () => {
+  document.getElementById("resourceCount").textContent = "Sugar Ready For Collecting: " + FormatNumbers(game.sugar);
 }
 
-resources.handleClick = () => {
-  game.resources++;
-  resources.updateResourceCount();
+sugar.startSugarGeneration = () => {
+  setInterval(() => {
+    game.sugar += game.multiplier; // Increment sugar by multiplier every second
+    sugar.updateSugarCount();
+  }, 1000); // 1000 milliseconds = 1 second
 }
 
+sugar.collectSugar = () => {
+  game.resourcesCollected = game.sugar; // Store the sugar waiting to be collected
+  game.sugar = 0; // Reset the sugar to 0
+  sugar.updateSugarCount();
+  alert("You collected " + game.resourcesCollected + " sugar!"); // Inform the user about collected sugar
+}
 
 document.getElementById("clickButton").addEventListener("mousedown", () => {
-  resources.handleClick()
-})
-render.renderFunctions.push(() => {
-  resources.updateResourceCount()
-})
+  sugar.collectSugar(); // Change to collectSugar function
+});
+
+sugar.startSugarGeneration(); // Start sugar generation
