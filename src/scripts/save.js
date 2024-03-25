4e87d783b5eb23531d9757d432d9e8a1a9233e33
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-03-24 19:55:39 -0500

    fix thing or smth

--- src/scripts/save.js
+++ src/scripts/save.js
@@ -16,7 +16,7 @@ saving.resetSave = ()=>{
     // todo: Make this into an actual popup instead of basic alerts
     if(prompt("Are you sure you want to reset you save and lose all progress? Type 'yes' to confirm")){
         localStorage.removeItem("save")
-        window.location.assign("/")
+        window.location.reload(true)
     }
 }
 
