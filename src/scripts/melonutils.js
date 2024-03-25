e7c3d395bb51bbe1edaf822a0557179fe54c7e4e
codemob <69110900+commandblox@users.noreply.github.com>
2024-03-25 12:06:14 -0400

    :3

---
+++ src/scripts/melonutils.js
@@ -0,0 +1,78 @@
+// FormatNumbers
+function FormatNumbers(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// Get Random
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Save Cookies
+function saveToCookie(key, value) {
+    document.cookie = `${key}=${JSON.stringify(value)}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+}
+
+// Get da cookies
+function getFromCookie(key) {
+    const cookies = document.cookie.split(';');
+    for (const cookie of cookies) {
+        const [cookieKey, cookieValue] = cookie.split('=');
+        if (cookieKey.trim() === key) {
+            return JSON.parse(cookieValue);
+        }
+    }
+    return null;
+}
+
+// Delete za cookies
+function deleteCookie(key) {
+    document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+}
+
+// is numbery
+function isNumeric(value) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+}
+
+// idk some stack overflow thing said this is useful sometimes
+function debounce(func, delay) {
+    let timeoutId;
+    return function(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
+
+// Throttle go brrrrr
+function throttle(func, limit) {
+    let inThrottle;
+    return function(...args) {
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    };
+}
+
+function getCookieCount() {
+    return document.cookie.split(';').length;
+}
+
+
+function getStoreName() {
+    return localStorage.getItem('storeName');
+}
+
+function popup(message) {
+    alert(message);
+}
+
+function formatDate(date) {
+    return new Date(date).toLocaleDateString();
+}
+
--- src/scripts/resources.js
+++ src/scripts/resources.js
@@ -1,12 +1,29 @@
-addProperty(game,"resources",0)
 
-function updateResourceCount() {
-    document.getElementById("resourceCount").textContent = "Resources: " + game.resources;
+
+addProperty(game, "sugar", 0); // Add sugar property to the game object
+
+var sugar = {};
+
+sugar.updateSugarCount = () => {
+  document.getElementById("resourceCount").textContent = "Sugar Ready For Collecting: " + FormatNumbers(game.sugar);
 }
 
-function handleClick() {
-    game.resources++;
-    updateResourceCount();
+sugar.startSugarGeneration = () => {
+  setInterval(() => {
+    game.sugar += game.multiplier; // Increment sugar by multiplier every second
+    sugar.updateSugarCount();
+  }, 1000); // 1000 milliseconds = 1 second
 }
 
-document.getElementById("clickButton").addEventListener("click", handleClick);
+sugar.collectSugar = () => {
+  game.resourcesCollected = game.sugar; // Store the sugar waiting to be collected
+  game.sugar = 0; // Reset the sugar to 0
+  sugar.updateSugarCount();
+  alert("You collected " + game.resourcesCollected + " sugar!"); // Inform the user about collected sugar
+}
+
+document.getElementById("clickButton").addEventListener("mousedown", () => {
+  sugar.collectSugar(); // Change to collectSugar function
+});
+
+sugar.startSugarGeneration(); // Start sugar generation
