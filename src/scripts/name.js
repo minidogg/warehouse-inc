c7a27290b8bdb9a3a2c8519689a153f5976b2dbf
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-03-25 17:21:42 -0500

    ee

--- src/scripts/name.js
+++ src/scripts/name.js
@@ -3,13 +3,19 @@ function randomName(overwrite=false) {
   var nameAdjs = ['Tired', 'High', 'Hungry', 'Agitated', 'Excited', 'Grumpy', 'Sleepy', 'Angry', 'Bored', 'Thirsty', 'Extremely High', 'Illegal', 'Scamming', 'Scammed', 'Screaming', 'Drug Promoting', 'Your Friendly',"Sugar Smuggling"];
   var nameNouns = ['Cartel Leader', 'Addict', 'Mafia Boss', 'Discord Moderator', 'Internet Troll', 'Slacker', 'Loser', 'Scammer', 'Karen', 'Drug Lord', 'Mother', 'Minor', 'Sugar Addict',"Mafia"];
 
+  var goldenNames = ["Sugar Smuggling Minor"]
+
   if (overwrite) removeProperty(game, "name");
   addProperty(game, "name", nameAdjs[Math.floor(Math.random() * nameAdjs.length)] + ' ' + nameNouns[Math.floor(Math.random() * nameNouns.length)]);
+  addProperty(game, "goldenName",goldenNames.includes(game.name) );
+
 
   saving.save()
 }
 render.renderFunctions.push(() => {
   document.getElementById('companyName').textContent = game.name + "'s Warehouse"
+
+  if(game.goldenName==true)document.getElementById('companyName').style.color = "gold"
 })
 
 // todo: update this to an actual prompt
