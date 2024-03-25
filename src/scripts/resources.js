a9dfccdfdb396a9e618f474efd8369dcfcec329c
Crafterboy27-school <95499245+Crafterboy27-school@users.noreply.github.com>
2024-03-25 08:20:39 -0500

    Update resources.js

--- src/scripts/resources.js
+++ src/scripts/resources.js
@@ -9,4 +9,6 @@ function handleClick() {
     updateResourceCount();
 }
 
-document.getElementById("clickButton").addEventListener("click", handleClick);
+document.getElementById("clickButton").addEventListener("mousedown", () => {
+  handleClick()
+})
