27b3db0bf2cd69ce2f2e075f68204804f09ccc2c
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-03-25 19:15:21 -0500

    popup :D

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -9,6 +9,7 @@ function colorLog(text,color="lime",fontSize="15px"){
 
 document.body.scrollTop = document.body.scrollHeight;
 
+const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
 
 showNotification = (message) => {
   const notificationElement = document.getElementById("notification");
@@ -19,3 +20,24 @@ showNotification = (message) => {
     notificationElement.classList.remove("notification-show");
   }, 3000); // Adjust the duration here (in milliseconds)
 }
+
+
+document.querySelector("#popup").style.display = "none"
+
+showPopup = (title,html,callback)=>{
+    document.querySelector("#popup").style.display = "block"
+    document.querySelector("#popupHeader").textContent = title
+    document.querySelector("#popupHtml").innerHTML = ""
+    document.querySelector("#popupHtml").appendChild(html)
+
+    var listener = document.body.querySelector("#popupSubmit").onclick = (ev)=>{
+        callback(ev)
+        document.querySelector("#popup").style.display = "none"
+    }
+
+}
+let testEl = document.createElement("h1")
+testEl.innerHTML = "Hello!"
+showPopup("This is a title",testEl,()=>{
+    console.log("Popup submitted :D")
+})
