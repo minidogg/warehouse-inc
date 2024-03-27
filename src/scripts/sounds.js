416a1139deef8ca492dfe97b411d5189936cff15
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-03-27 12:41:20 -0600

    YOOO, BG AND MUSIC!

--- src/scripts/sounds.js
+++ src/scripts/sounds.js
@@ -1,12 +1,13 @@
-window.onload = setInterval(GameLoop, 1000 / 10); //10fps
+/*  window.onload = setInterval(GameLoop, 1000 / 10); //10fps
 
-let BGAudio = new Audio();
+    let BGAudio = new Audio();
 
-BGAudio.src = '../sfx/music/FastMenuLiteHeavy.mp3';
+    BGAudio.src = '../sfx/music/FastMenuLiteHeavy.mp3';
 
-function Gameloop() {
-    BGAudio.play();
-    if (BGAudio.paused == true) {
+    function Gameloop() {
         BGAudio.play();
+        if (BGAudio.paused == true) {
+            BGAudio.play();
+        }
     }
-}
+*/
