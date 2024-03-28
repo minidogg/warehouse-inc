6d35b51286fb5b09213be8fc1394bb1988490c78
codemob <69110900+commandblox@users.noreply.github.com>
2024-03-28 11:05:05 -0400

    boykisser crap :3

--- crapmessages.js
+++ crapmessages.js
@@ -1,4 +1,5 @@
 const crapMessages = [
 'cool feature, huh? :3', 
-'<span style="color:#6bc5b9; font-size:8vmin; font-family:\'Comic Sans MS\', \'Comic Sans\', cursive">E</span>'
+'<span style="color:#6bc5b9; font-size:8vmin; font-family:\'Comic Sans MS\', \'Comic Sans\', cursive">E</span>', 
+'you like kissing boys, <span style="font-size:4vmin;">don\'t you?</span> (owo)'
 ];
