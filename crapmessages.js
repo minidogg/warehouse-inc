ad720df93f99163011232fd842ded935a16cbdaf
codemob <69110900+commandblox@users.noreply.github.com>
2024-03-29 07:57:51 -0400

    more crap

--- crapmessages.js
+++ crapmessages.js
@@ -1,5 +1,22 @@
 const crapMessages = [
 'cool feature, huh? :3', 
 '<span style="color:#6bc5b9; font-size:8vmin; font-family:\'Comic Sans MS\', \'Comic Sans\', cursive">E</span>', 
-'you like kissing boys, <span style="font-size:4vmin;">don\'t you?</span> (owo)'
+'you like kissing boys, <span style="font-size:4vmin;">don\'t you?</span> (owo)',
+'<span style="color:#6bc5b9; font-family:\'Comic Sans MS\', \'Comic Sans\', cursive">Comic sans is the superior font</span>',
+`
+<style>
+#spinny {
+    animation: spin 1s infinite linear;
+}
+@keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
+<div style="font-size:8vmin" id=spinny>SPIN</div>
+`
 ];
