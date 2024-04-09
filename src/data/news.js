e089c7e94a2c61a1a323fc167098b34f7917cd9c
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-04-08 19:02:04 -0600

    my click icon :(

--- src/data/news.js
+++ src/data/news.js
@@ -1,5 +1,4 @@
 const newsData = [
     { threshold: 100, content: "News for users with less than 100 sugar" },
     { threshold: 500, content: "News for users with less than 500 sugar" },
-    // Add more news items as needed
 ];
