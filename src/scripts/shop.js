aa1d77ddbfc45fa1feddfab123d77d37b0bb266d
codemob <69110900+commandblox@users.noreply.github.com>
2024-04-09 06:17:21 -0400

    :3 i did thinge

--- src/scripts/shop.js
+++ src/scripts/shop.js
@@ -28,6 +28,8 @@ shop.compileNum = genRanHex(16);
 shop.findCost = (baseCost, costMultiplier, numOwned) =>
     Math.floor(baseCost * Math.pow(1 + costMultiplier, numOwned));
 
+addProperty(game, "currentShopVisible", 3);
+
 Object.entries(game.producers).forEach((producerNameData) => {
     let id = producerNameData[0];
     let data = producerNameData[1];
@@ -37,18 +39,19 @@ Object.entries(game.producers).forEach((producerNameData) => {
         <h1 class="buyOptionName">Loading your mother... this will only take 69.420 nanoseconds...</h1>
         <span class="buyOptionPrice">$69420 - 42069 SPS - 69 Owned</span>
     `;
+
+    q("#buyMenu").appendChild(shopItemElement);
+
     shopItemElement.classList.add("buyOption");
     shopItemElement.q(".buyOptionName").textContent = data.name;
     shopItemElement.q(".buyOptionPrice").textContent = `${abbreviateNumber(shop.findCost(data.baseCost, data.costMultiplier, data.owned))} sugar - ${data.sps} SPS - ${data.owned} Owned`;
 
     shopItemElement.id = shop.compileNum + "_buyOption_" + id;
 
-    q("#buyMenu").appendChild(shopItemElement);
-
     shopItemElement.onclick = () => {
         let producerData = game.producers[id];
         let cost = shop.findCost(producerData.baseCost, producerData.costMultiplier, producerData.owned);
-        if (game.sugar <= cost) showNotification("Not enough money for: '" + producerData.name + "'");
+        if (cost > game.sugar) {showNotification(`Not enough money for "${producerData.name}"`);return}
 
         game.sugar -= cost;
         game.producers[id].owned += 1;
@@ -58,19 +61,33 @@ Object.entries(game.producers).forEach((producerNameData) => {
 });
 
 shop.update = () => {
-    Object.entries(game.producers).forEach((ee) => {
+    let i = 0;
+    for (ee of Object.entries(game.producers)) {
         let id = ee[0];
         let data = ee[1];
         let cost = shop.findCost(data.baseCost, data.costMultiplier, data.owned);
 
         let el = document.getElementById(shop.compileNum + "_buyOption_" + id);
 
-        el.classList.add("buyOptionDisabled");
-        if (game.sugar >= cost) el.classList.remove("buyOptionDisabled");
+        if (i < game.currentShopVisible) {
+            el.classList.remove("buyOptionUnknown", "buyOptionDisabled");
+            if (cost > game.sugar) {
+                el.classList.add("buyOptionDisabled");
+            } else {
+                game.currentShopVisible = Math.max(game.currentShopVisible, i+2)
+            }
 
-        el.q(".buyOptionName").textContent = data.name;
-        el.q(".buyOptionPrice").textContent = `${abbreviateNumber(cost)} sugar - ${data.sps} SPS - ${data.owned} Owned`;
-    });
+            el.q(".buyOptionName").textContent = data.name;
+            el.q(".buyOptionPrice").textContent = `${abbreviateNumber(cost)} sugar - ${data.sps} SPS - ${data.owned} Owned`;
+        } else {
+            el.classList.remove("buyOptionDisabled");
+            el.classList.add("buyOptionUnknown");
+
+            el.q(".buyOptionName").textContent = "???";
+            el.q(".buyOptionPrice").textContent = `${abbreviateNumber(cost)} sugar - ??? SPS - 0 Owned`;
+        }
+        i++;
+    }
 };
 
 render.renderFunctions.push(() => {
