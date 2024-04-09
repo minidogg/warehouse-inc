8717a0f74740b731eda94e88e79e1d0b5e2e1276
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-04-09 16:45:21 -0600

    e

--- src/scripts/news.js
+++ src/scripts/news.js
@@ -13,5 +13,5 @@ function updateNews(totalSugar) {
 
 const newsData = [
     { threshold: 100, content: "News for users with less than 100 sugar" },
-    { threshold: 500, content: "News for users with less than 500 sugar" },
+    { threshold: 500, content: "News for users with more than 500 sugar" },
 ];
