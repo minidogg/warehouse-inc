2ac2cfada8a3eee54e3bcb5f2e0781f222bd1726
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-04-10 16:31:35 -0500

    wip

--- src/scripts/minigames.js
+++ src/scripts/minigames.js
@@ -1,1 +1,27 @@
-colorLog("Hello from the minigames.js!","color:green;")
+var minigames = {}
+minigames.open = ()=>{
+    q("#minigames").style.display = "block"
+}
+minigames.main = async()=>{
+    colorLog("Hello from the minigames.js!","lime")
+
+    minigames.minigames = await fetch("./minigames/data.json")
+    minigames.minigames = await minigames.minigames.json()
+
+    minigames.minigames.forEach(e=> {
+        let button = document.createElement("button")
+        button.textContent = e.name
+
+        button.onclick = ()=>{
+
+        }
+
+        q("#minigameList").appendChild(button)
+    });
+
+    menu.menus.minigames = minigames.open
+    menu.updateMenus()
+    
+    minigames.open()   
+}
+minigames.main()
--- src/scripts/util.js
+++ src/scripts/util.js
@@ -1,9 +1,10 @@
 // This script should NEVER include any utils related to the game itself.
 // Instead this should be used for general utils like the one below.
-function colorLog(text,color="lime",fontSize="15px"){
+function colorLog(text,color="lime",fontSize="15px",style=""){
     console.log("%c "+text, `
         color:${color};
         font-size:${fontSize};
+        ${style}
     `);
 }
 
@@ -46,6 +47,7 @@ showPopup = (title,html,callback)=>{
     }
 
 }
+
 showPrompt = async (title,placeholder="",maxLen=-1)=>{
     return await new Promise((resolve, reject) => {
         let input = document.createElement("input")
