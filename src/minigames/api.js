daf9ae1d38655127bc47cf8e3c2f527cae4c2c27
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-04-10 20:26:47 -0500

    api asafsjhjd

--- src/minigames/api.js
+++ src/minigames/api.js
@@ -2,10 +2,10 @@ var api = {}
 api.onReady = []
 var game = {}
 window.addEventListener("message",e=>{
-    if(e.data.type=="game"){
-        game=e.data.data.game
-        return
-    }
+    // if(e.data.type=="game"){
+    //     game=e.data.data.game
+    //     return
+    // }
     if(e.data.type=="ready"){
         api.onReady.forEach((e)=>{
             e()
@@ -14,3 +14,21 @@ window.addEventListener("message",e=>{
     }
 })
 
+api.post = (type,data)=>{
+    window.top.postMessage({"type":type,"data":data}, '*');
+}
+
+api.request = {}
+api.request.game = async ()=>{
+    return await new Promise(async(resolve,reject)=>{
+        window.top.postMessage({"type":"requestGame","data":{}}, '*');
+        let tempListener = window.addEventListener("message",e=>{
+            game = e.data.data.game
+            resolve(game)
+        })
+    })
+}
+api.do = {}
+api.do.addSugar = (sugar)=>{
+    api.post("addSugar",{amount:sugar})
+}
