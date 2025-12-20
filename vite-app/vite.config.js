b4880ef06920fa6f2cef68c52824beb5d132f4c0
minidogg <117037081+minidogg@users.noreply.github.com>
2025-12-20 10:57:23 -0600

    the start of the holy refactor

---
+++ vite-app/vite.config.js
@@ -0,0 +1,14 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "osrc/index.html"),
+      },
+    },
+  },
+})
