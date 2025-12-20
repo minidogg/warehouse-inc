e99fcde66f2b95de23c75f0c3ce7ce42e7977f11
minidogg <117037081+minidogg@users.noreply.github.com>
2025-12-20 11:09:11 -0600

    home page somewhat functional

---
+++ vite-app/src/database/data/news.js
@@ -0,0 +1,4 @@
+const newsData = [
+  { threshold: 100, content: "News for users with less than 100 sugar" },
+  { threshold: 500, content: "News for users with less than 500 sugar" },
+];
---
+++ vite-app/src/home.js
@@ -0,0 +1,36 @@
+import '../src/stylesheets/index.css'
+import { crapMessages } from './database/data/crapmessages';
+const firstCrapDate = new Date(2024, 2, 28);
+let daysSinceCrapCreation = Math.floor(
+    (new Date() - firstCrapDate) / (1000 * 60),
+); // et daysSinceCrapCreation = Math.floor((new Date() - firstCrapDate) / (1000 * 3600 * 24));
+const crapContents = document.getElementById("crapContents");
+
+crapContents.innerHTML =
+crapMessages[daysSinceCrapCreation % crapMessages.length];
+
+function switchTo(site) {
+    const bgscroll = document.getElementById("bgscroll");
+    const container = document.getElementById("container");
+
+    container.style.transform = "translateY(100vh)";
+    bgscroll.style.backgroundPositionX =
+        getComputedStyle(bgscroll).backgroundPositionX;
+    bgscroll.style.animation = "fadeout 1s";
+
+    container.style.transform = "translateY(100vh)";
+    setTimeout(() => {
+        window.location.href = site;
+    }, 1100);
+    }
+
+    const bgMusic = new Audio("/sfx/music/FastMenuLiteHeavy.mp3");
+
+    document.addEventListener("DOMContentLoaded", async function () {
+    try {
+        bgMusic.loop = true;
+        await bgMusic.play();
+    } catch {
+        document.getElementById("autoplayPopup").style.display = "unset";
+    }
+});
